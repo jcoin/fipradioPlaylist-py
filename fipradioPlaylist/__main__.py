16d2588725339dfb1b82f5769b65717dde70bcf4
J coin <jrlamoule@gmail.com>
2019-07-16 17:56:38 +0200

    Test 1

---
+++ fipradioPlaylist/__main__.py
@@ -0,0 +1,1 @@
+from fipradioPlaylist.client import fipplaylistClient
--- setup.py
+++ setup.py
@@ -1,6 +1,18 @@
-setup(name="fipradio-playlist",
-      version="1.0",
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(name="fipradioPlaylist",
+      version="0.1.0",
       author="jcoin",
+      author_email
+      long_description=README,
+      long_description_content_type="text/markdown",
       description="Library to access the previous, current and future tracks "
                   "from FIP Radio & its webradio",
       url="https://github.com/jcoin/fipradio-playlist",
@@ -9,6 +21,7 @@ setup(name="fipradio-playlist",
                    "License :: OSI Approved :: MIT License",
                    "Programming Language :: Python :: 3",
                    "Programming Language :: Python :: 3 :: Only"],
-      install_requires=['aiohttp'],
+      install_requires=['requests'],
+      packages=["reader"],
       py_modules=['fipradio-playlist'],
       scripts=[])
